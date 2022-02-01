528688885f9d0d2d1e9b43efef260248ec6deaf3
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-02-01 20:01:54 -0300

    so abalo

---
+++ resources/js/components/TesteVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Aqui estou fazendo meus testes de vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TesteVue',
+}
+</script>
+
+<style>
+
+  div {
+    background-color: red;
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.index')
+
+@section('cabecalho')
+    <header>
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+            <nav>
+                <a href="">Sobre</a>
+                <a href="">Contato</a>
+                <a href="">Fale comigo</a>
+            </nav>
+        </div>
+    </header>
+@endsection
